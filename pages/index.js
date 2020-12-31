f68f0b6b386de0d92630caa9137017c4ef967e88
Alyssa <williamsalyssa1@gmail.com>
2020-12-30 21:17:17 -0500

    feat: Inital commit for Assignment 3

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,107 @@
+import axios from 'axios';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [listPlanets, setListPlanets] = useState([]);
+  let planetMaker = p => <div className="grid flex flex-center justify-center bg-green-300 text-center"><div className = "text-xl pt-5">Planet: <b>{p.name}</b></div><div>Population: {p.population}</div></div>
+  const popFilter = (x => x.population != "unknown");
+  const knownPopulations = listPlanets.filter(popFilter);
+
+
+  useEffect(async() => {
+    const getPlanet = () => 
+      axios(
+        "https://swapi.dev/api/planets/"
+    ).then((response) => response.data.results);
+
+    getPlanet().then((planets) => {
+      console.log(planets)
+      setListPlanets(planets);
+    })
+
+  }, []);
+  
+  return(
+  <div>
+    <div className="pt-5 text-center text-3xl bg-green-300">Star Wars Planets with Known Populations</div>
+    <div>
+      {knownPopulations.map(planetMaker)}
+      </div>
+  </div>
+  )
+
+
+  /*const [apiResponse, setApiResponse] = useState("");
+  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState(""); // changes when we hit the "submit" button. allows our useEffect to be called any time we press the submit button (because we pass search as a dependency to useEffect)
+  const [isLoading, setIsLoading] = useState(false); // when isLoading is true we display our spinner loading icon
+
+  useEffect(() => {
+    setIsLoading(true); // set isLoading to TRUE before+during our API call
+
+    // for pulling the quote
+    const getQuote = () =>
+      axios(
+        "http://slowwly.robertomurray.co.uk/delay/3000/url/https://api.kanye.rest/"
+      ).then((response) => response.data.quote);
+
+    // call getQuote and store its value (which is a promise) in result
+    getQuote().then((quote) => {
+      setApiResponse(quote);
+      setIsLoading(false); // when the API call is done
+    });
+  }, [search]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Kanye8Ball</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h1 className={styles.title}>Kany8Ball</h1>
+        <br></br>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a className={styles.card}>
+            {isLoading ? (
+              <img
+                style={{ width: "50px", margin: "80px auto" }}
+                src="https://thumbs.gfycat.com/SkinnySeveralAsianlion.webp"
+                alt="loading..."
+              />
+            ) : (
+              <p className={styles.description}>{apiResponse}</p>
+            )}
           </a>
         </div>
+        <form
+          style={{ color: "black" }}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <label>
+            Name:
+            <input
+              type="text"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+            />{" "}
+          </label>
+          <input
+            type="submit"
+            value="Submit"
+            onClick={() => {
+              setSearch(query);
+            }}
+          />
+        </form>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <footer className={styles.footer}>Powered by APIs!</footer>
     </div>
-  )
+  );
+  */
 }
